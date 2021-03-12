d3291cbe8eaa03708fdee7ff71d3db5ed11cceb3
Veera Sivarajan <sveera.2001@gmail.com>
2021-03-12 14:53:26 -0500

    Add highlighting for java style comments
    Java style comments "/* ... */" work correctly. Can also be evoked
    from Alt + ; with proper highlighting.

--- syntax.el
+++ syntax.el
@@ -1,68 +1,73 @@
-;; (defvar duotone-font-lock-defaults
-;;   '((
-;;      ("\\<\\([A-Z]*[a-z0-9_]+\\)\\([ \t]*\\)(" . font-lock-function-name-face)))
-;;      ;; ("\"\\.\\*\\?" . font-lock-variable-name-face))
-;;     ) 
+(defvar mydsl-mode-syntax-table nil "Syntax table")
 
-
-
-;; (define-derived-mode duotone-mode prog-mode "C"
-;;   "major mode for editing mymath language code."
-;;   (setq font-lock-defaults '(duotone-font-lock-defaults))) 
+(setq mydsl-mode-syntax-table
+      (let ( (synTable (make-syntax-table)))
+        (modify-syntax-entry ?\/ "<" synTable)
+        (modify-syntax-entry ?\n ">" synTable) 
+        synTable))
 
 ;; (provide 'duoton-mode) 
 (defvar mydsl-constants
-    '("reservedword1"
-      "reservedword2"))
+  '("reservedword1"
+    "reservedword2"))
+
+(defvar mydsl-keywords
+  '("auto" "break" "case" "char" "const" "continue" "default" "do" "double"
+    "else" "enum" "extern" "float" "for" "goto" "if" "int" "long" "register"
+    "return" "short" "signed" "sizeof" "static" "struct" "switch" "typedef"
+    "union" "unsigned" "void" "volatile" "while")) 
+
+;; I'd probably put in a default that you want, as opposed to nil
+(defvar mydsl-tab-width nil "Width of a tab for MYDSL mode") 
+(setq mydsl-tab-width 4) 
+
+;; Two small edits.
+;; First is to put an extra set of parens () around the list
+;; which is the format that font-lock-defaults wants
+;; Second, you used ' (quote) at the outermost level where you wanted ` (backquote)
+;; you were very close
+(defvar mydsl-font-lock-defaults
+  `((
+     ;; stuff between double quotes
+     ;; ("\"\\.\\*\\?" . font-lock-string-face)
+     ;; ; : , ; { } =>  @ $ = are all special elements
+     ( ,(regexp-opt mydsl-keywords 'words) . font-lock-builtin-face)
+     ("\\<\\([A-Z]*[a-z0-9_]+\\)\\([ \t]*\\)(" . (1. font-lock-function-name-face))
+     ( ,(regexp-opt mydsl-constants 'words) . font-lock-constant-face)
+     ) oladfadf))
+
+(define-derived-mode mydsl-mode prog-mode "MYDSL script"
+  "MYDSL mode is a major mode for editing MYDSL files"
+  ;; you again used quote when you had '((mydsl-hilite))
+  ;; I just updated the variable to have the proper nesting (as noted above)
+  ;; and use the value directly here
+  (setq font-lock-defaults mydsl-font-lock-defaults)
   
-  (defvar mydsl-keywords
-    '("auto" "break" "case" "char" "const" "continue" "default" "do" "double"
-      "else" "enum" "extern" "float" "for" "goto" "if" "int" "long" "register"
-      "return" "short" "signed" "sizeof" "static" "struct" "switch" "typedef"
-      "union" "unsigned" "void" "volatile" "while")) 
+  ;; when there's an override, use it
+  ;; otherwise it gets the default value
+  (when mydsl-tab-width
+    (setq tab-width mydsl-tab-width))
   
-  ;; I'd probably put in a default that you want, as opposed to nil
-  (defvar mydsl-tab-width 4 "Width of a tab for MYDSL mode") 
+  (set-syntax-table my-mode-syntax-table)
+  (setq-local comment-start "/*")
+  (setq-local comment-start-skip "/\\*+[ \t]*")
+  (setq-local comment-end "*/")
+  (setq-local comment-end-skip "[ \t]*\\*+/")
+  ;; for comments
+  ;; overriding these vars gets you what (I think) you want
+  ;; they're made buffer local when you set them
+  ;; (setq comment-start "/*")
+  ;; (font-lock-add-keywords nil '(("/\\*\\(?:[^*]\\|\\*[^/]\\)*\\*+/" . font-lock-comment-face))) 
+  ;; (setq comment-end "*/")
   
-  ;; Two small edits.
-  ;; First is to put an extra set of parens () around the list
-  ;; which is the format that font-lock-defaults wants
-  ;; Second, you used ' (quote) at the outermost level where you wanted ` (backquote)
-  ;; you were very close
-  (defvar mydsl-font-lock-defaults
-    `((
-       ;; stuff between double quotes
-       ;; ("\"\\.\\*\\?" . font-lock-string-face)
-       ;; ; : , ; { } =>  @ $ = are all special elements
-       ( ,(regexp-opt mydsl-keywords 'words) . font-lock-builtin-face)
-       ("\\<\\([A-Z]*[a-z0-9_]+\\)\\([ \t]*\\)(" . (1. font-lock-function-name-face))
-       ( ,(regexp-opt mydsl-constants 'words) . font-lock-constant-face)
-       ) oladfadf))
+  ;; for comments like / ... /
+
+  ;; for comments like /* ...  */
+  (modify-syntax-entry ?\/ ". 14" mydsl-mode-syntax-table)
+  (modify-syntax-entry ?* ". 23" mydsl-mode-syntax-table)
   
-  (define-derived-mode mydsl-mode c-mode "MYDSL script"
-    "MYDSL mode is a major mode for editing MYDSL files"
-    ;; you again used quote when you had '((mydsl-hilite))
-    ;; I just updated the variable to have the proper nesting (as noted above)
-    ;; and use the value directly here
-    (setq font-lock-defaults mydsl-font-lock-defaults)
-  
-    ;; when there's an override, use it
-    ;; otherwise it gets the default value
-    (when mydsl-tab-width
-      (setq tab-width mydsl-tab-width))
-  
-    ;; for comments
-    ;; overriding these vars gets you what (I think) you want
-    ;; they're made buffer local when you set them
-    ;; (setq comment-start "//")
-    (font-lock-add-keywords nil '(("/\\*\\(?:[^*]\\|\\*[^/]\\)*\\*+/" . font-lock-comment-face))) 
-    ;; (setq comment-end "")
-  
-    (modify-syntax-entry ?# "< b" mydsl-mode-syntax-table)
-    (modify-syntax-entry ?\n "> b" mydsl-mode-syntax-table)
-  
-    ;; Note that there's no need to manually call `mydsl-mode-hook'; `define-derived-mode'
-    ;; will define `mydsl-mode' to call it properly right before it exits
-    )
-  
-  (provide 'mydsl-mode)
+  ;; Note that there's no need to manually call `mydsl-mode-hook'; `define-derived-mode'
+  ;; will define `mydsl-mode' to call it properly right before it exits
+  )
+
+(provide 'mydsl-mode)
--- test.el
+++ test.el
@@ -1,17 +1,37 @@
 (defvar xpy-mode-syntax-table nil "Syntax table for `xpy-mode'.")
 
+;; (defvar xpy-keywords
+;;   '("auto" "break" "case" "char" "const" "continue" "default" "do" "double"
+;;     "else" "enum" "extern" "float" "for" "goto" "if" "int" "long" "register"
+;;     "return" "short" "signed" "sizeof" "static" "struct" "switch" "typedef"
+;;     "union" "unsigned" "void" "volatile" "while")) 
+
+;; (defvar xpy-font-lock-defaults
+;;   `((
+;;      ;; stuff between double quotes
+;;      ;; ("\"\\.\\*\\?" . font-lock-string-face)
+;;      ;; ; : , ; { } =>  @ $ = are all special elements
+;;      ( ,(regexp-opt xpy-keywords 'words) . font-lock-builtin-face)
+;;      ("\\<\\([A-Z]*[a-z0-9_]+\\)\\([ \t]*\\)(" . (1. font-lock-function-name-face))
+;;      ) oladfadf))
+
 (setq xpy-mode-syntax-table
       (let ( (synTable (make-syntax-table)))
         ;; python style comment: “# …”
-        (modify-syntax-entry ?\/ "<" synTable)
-        (modify-syntax-entry ?\n ">" synTable)
+        (modify-syntax-entry ?\/ ". 12b" synTable)
+        (modify-syntax-entry ?\n "> b" synTable)
+        (modify-syntax-entry ?\/ ". 14" synTable)
+        (modify-syntax-entry ?* ". 23" synTable)
         synTable))
 
 (define-derived-mode xpy-mode prog-mode "xpy"
   "xpy-mode is a major mode for editing language xpy."
 
-  (setq font-lock-defaults (list nil nil))
-
-  (set-syntax-table xpy-mode-syntax-table)
+  ;; (setq font-lock-defaults xpy-font-lock-defaults)
+  (set-syntax-table xpy-mode-syntax-table) 
+  (setq-local comment-start "/*")
+  (setq-local comment-start-skip "/\\*+[ \t]*")
+  (setq-local comment-end "*/")
+  (setq-local comment-end-skip "[ \t]*\\*+/")
   ;; actually no need, because our syntax table name is “xpy-mode” + “-syntax-table”, so define-derived-mode will find it and set it
   )
