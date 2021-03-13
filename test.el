e109579477ca83d0378ca4e82d08a911e4bfd779
Veera Sivarajan <sveera.2001@gmail.com>
2021-03-13 15:01:37 -0500

    Add custom face for comments
    But does not add regex for matching comments

--- test.el
+++ test.el
@@ -1,54 +1,62 @@
-(defvar xpy-mode-syntax-table nil "Syntax table for `xpy-mode'.")
+(defvar minc-mode-syntax-table nil "Syntax table for `minc-mode'.")
 
-(defvar xpy-keywords
+(defvar minc-keywords
   '("auto" "break" "case" "char" "const" "continue" "default" "do" "double"
     "else" "enum" "extern" "float" "for" "goto" "if" "int" "long" "register"
     "return" "short" "signed" "sizeof" "static" "struct" "switch" "typedef"
     "union" "unsigned" "void" "volatile" "while")) 
 
-(defface xpy-keywords-face
+(defface minc-keywords-face
   '((t :foreground "gray"
        ))
-  "Face"
-  :group 'xpy-mode)  
+  "Keywords Face"
+  :group 'minc-mode)  
 
-(defface xpy-function-face
+(defface minc-function-face
   '((t :foreground "white"
        :weight ultra-bold
        :width extra-expanded
        ))
-  "Face"
-  :group 'xpy-mode)  
+  "Function Face"
+  :group 'minc-mode)  
+
+(defface minc-comment-face
+  '((t :foreground "#7a7272"
+       :slat italic
+       ))
+  "Comment Face"
+  :group 'minc-mode)
 
 
-(defvar xpy-keywords-face 'xpy-keywords-face)
-(defvar xpy-function-face 'xpy-function-face) 
+(defvar minc-keywords-face 'minc-keywords-face)
+(defvar minc-function-face 'minc-function-face) 
+(defvar minc-comment-face 'minc-comment-face) 
 
-(defvar xpy-font-lock-defaults
+(defvar minc-font-lock-defaults
   `((
      ;; stuff between double quotes
      ;; ("\"\\.\\*\\?" . font-lock-string-face)
      ;; ; : , ; { } =>  @ $ = are all special elements
      ( "\\<\\([0-9]*\\.[0-9]+\\|[0-9]+\\)[df]?\\>" . font-lock-string-face) 
-     ( ,(regexp-opt xpy-keywords 'words) . xpy-keywords-face) 
-     ("\\<\\([A-Z]*[a-z0-9_]+\\)\\([ \t]*\\)(" . (1. xpy-function-face))
+     ( ,(regexp-opt minc-keywords 'words) . minc-keywords-face) 
+     ("\\<\\([A-Z]*[a-z0-9_]+\\)\\([ \t]*\\)(" . (1. minc-function-face))
      )))
 
-(setq xpy-mode-syntax-table
+(setq minc-mode-syntax-table
       (let ( (synTable (make-syntax-table)))
         ;; python style comment: “# …”
         (modify-syntax-entry ?\/ ". 12b" synTable)
         (modify-syntax-entry ?\n "> b" synTable)
         synTable)) 
 
-(define-derived-mode xpy-mode c-mode "xpy"
-  "xpy-mode is a major mode for editing language xpy."
+(define-derived-mode minc-mode c-mode "C"
+  "minc-mode is a major mode for editing language minc."
 
-  (set-syntax-table xpy-mode-syntax-table) 
-  (setq font-lock-defaults xpy-font-lock-defaults)
+  (set-syntax-table minc-mode-syntax-table) 
+  (setq font-lock-defaults minc-font-lock-defaults)
   (setq-local comment-start "//")
   (setq-local comment-start-skip "/\\*+[ \t]*")
   (setq-local comment-end "")
   (setq-local comment-end-skip "[ \t]*\\*+/")
-  ;; actually no need, because our syntax table name is “xpy-mode” + “-syntax-table”, so define-derived-mode will find it and set it
+  ;; actually no need, because our syntax table name is “minc-mode” + “-syntax-table”, so define-derived-mode will find it and set it
   )
